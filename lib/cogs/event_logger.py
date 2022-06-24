72836de24498234f902be242505bb865ba7e5d86
Mutim <taormino24@gmail.com>
2022-06-24 17:46:54 -0500

    Update event_logger.py

--- lib/cogs/event_logger.py
+++ lib/cogs/event_logger.py
@@ -223,7 +223,7 @@ class EventLogger(commands.Cog, name="Event Logger"):
         # user_data = {"_id": f"{message.author.id}"}, {'$push': {"message_ids": f"{message.id}"}}, {'upsert': True}
 
         try:
-            user_db.update_one({"_id": f"{message.author.id}"}, {'$push': {"message_ids": f"{message.id}"}}, {'upsert': True})
+            user_db.update_one({"_id": f"{message.author.id}"}, {'$push': {"message_ids": f"{message.id}"}}, upsert=True)
         except Exception as err:
             print(err)
             await self.bot.get_channel(989509544218611753).send(err)
