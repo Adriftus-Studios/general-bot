b9afff0b76ef4bd7a5615d2568d62c96a9965bec
Mutim <taormino24@gmail.com>
2022-06-24 15:17:51 -0500

    Updated DB handling
    Moved many functions from main.py, to event_logger.py
    
    Created cog for logging events to the DB.

---
+++ lib/cogs/event_logger.py
@@ -0,0 +1,325 @@
+
+import discord
+import pymongo.errors
+
+import config
+import time
+import datetime
+import aiofiles
+import random
+from secrets import MONGO_CLIENT
+
+from discord.ext import commands
+
+db_client = MONGO_CLIENT
+message_db = db_client.Messages
+user_db = db_client.Users
+minecraft_db = db_client.Minecraft
+
+
+class EventLogger(commands.Cog, name="Event Logger"):
+    """
+    A class cog that holds the EventLogger object.
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+        name of the cog, used in reporting
+
+    Methods
+    -------
+    message_to_discord(payload, message_data, payload_guild_id):
+        Sends a log message to discord.
+
+    serialize_to_db(message):
+        Writes data to the database for later retrieval.
+
+    deserialize_from_db(message):
+        Returns full message from the messages_db
+    """
+    def __init__(self, bot: commands.Bot) -> None:
+        """
+        Constructs all the necessary attributes for the EventLogger object.
+
+        Parameters
+        ----------
+            bot : DiscordObject
+                Discord object for the bot class
+        """
+        self.bot = bot
+
+    # Log Events --
+    @commands.Cog.listener()
+    async def on_message(self, message) -> None:
+
+        # Reporting channel (Avoid circular logs)
+        if message.channel.id == 989509544218611753:
+            return
+
+        try:
+            await self.serialize_to_db(message)
+
+        except Exception as err:
+            print(f"An error has occurred: {err}")
+
+    @commands.Cog.listener()
+    async def on_raw_message_delete(self, payload) -> None:
+        """
+        channel_id¶ [int]
+        The channel ID where the deletion took place.
+
+        guild_id [int]
+        The guild ID where the deletion took place, if applicable.
+
+        message_id [int]
+        The message ID that got deleted.
+
+        cached_message Optional[Message]
+        The cached message, if found in the internal message cache.
+        """
+        admin_channels = [626086306606350366, 651870920599928862]
+        blacklist_channels = []
+        # Send full db.Messages collection info to logging channel
+        try:
+            # message_data = self.serialize_to_db(payload)
+            message_data = message_db.find_one({"_id": f"{payload.id}"})
+            if payload.cached_message is not None:
+                message_data = message_data
+            elif message_db.find({'_id': f'{payload.message_id}'}).count() > 0:
+                message_data = await self.deserialize_from_db(payload)
+
+            if payload.channel_id in admin_channels:
+                await self.message_to_discord(payload, message_data, 712309385019523155)
+            elif payload.channel_id in blacklist_channels:
+                pass
+            else:
+                await self.message_to_discord(payload, message_data, 989509544218611753)
+        except Exception as err:
+            print(f'An error has occurred: {err}')
+
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(self, channel) -> None:
+        print(f"Channel Deleted - {channel}")
+
+    @commands.Cog.listener()
+    async def on_interaction(self, ctx: discord.Interaction):
+        try:
+            executed_command = ctx.command.name
+            message_content = f"""
+            - Executed **{executed_command}** command in {ctx.guild} 
+            - (ID: {ctx.guild.id}) by {ctx.user} (ID: {ctx.user.id})\n
+            ----------------------------------------------------------------\n
+            """
+            async with aiofiles.open("message_log.txt", "a") as f:
+                await f.writelines(message_content)
+
+            if len(message_content) > 1000:
+                message_content = f"{message_content[:1000]}...[Continued]"
+
+            embed = discord.Embed(
+                title=f"*** Command Log Entry ***",
+                description=f"○○○○○○○○○○○○○○○○○○○○○○○○○○○",
+                color=config.success)
+            embed.add_field(name=f"Command Executed - ", value=f"{message_content}", inline=False)
+            embed.set_footer(text=f"• {time.ctime(time.time())}")
+
+            try:
+                # Sends to discord-logs channel
+                await self.bot.get_channel(989509544218611753).send(embed=embed)
+            except Exception as err:
+                print(f'An error has occurred: {err}')
+            print(
+                f"Executed {executed_command} command in {ctx.guild}"
+                f"(ID: {ctx.guild.id}) by {ctx.user} (ID: {ctx.user.id})")
+        except Exception as err:
+            print(err)
+
+    async def message_to_discord(self, payload, message_data, send_channel):
+        """
+        Parameters
+        ----------
+            payload : channel_id [int], guild_id [Otional[int]], message_id [int], cached_message [Optional[Message]]
+                The channel ID where the deletion took place.
+
+            message_data : returned from EventLogger.serialize_to_db(message) [str]
+                Message Data information saved to a dictionary.
+
+            send_channel : channel_id [int]
+                Channel ID that the logged information will be sent to.
+                    Currently, this will go to #discord-logs (989509544218611753)
+
+        """
+        current_time = time.ctime(time.time())
+        guild = self.bot.get_guild(payload.guild_id)
+        deleter = ""
+        deleted_channel_id = 0
+        deleted_owner = 0
+
+        async for entry in guild.audit_logs(limit=1, action=discord.AuditLogAction.message_delete):
+            deleted_channel_id = entry.extra.channel.id
+            deleted_owner = entry.target.id
+            deleter = entry.user
+
+        # Change this to query the db
+        if int(payload.channel_id) != int(deleted_channel_id) or int(message_data["author_id"]) != int(deleted_owner):
+            deleter = self.bot.get_user(int(message_data["author_id"]))
+
+        message_content = f"""
+                *** Message Log Entry - {current_time} ***
+                - {message_data["author_full_name"]} had their message deleted -
+                  **__Deleted By__**: {deleter}#{deleter.discriminator}
+                  **__Message Author__**: {message_data["author_full_name"]}
+                  **__Author ID__**: {message_data["author_id"]}
+                  **__Channel Info__**: <#{message_data["channel_id"]}> / {message_data["channel_id"]}
+                  **__Server Name__**: {message_data["server_name"]}
+
+                  **__Message:__**\n{message_data["contents"]}
+                ----------------------------------------------------------------\n
+                """
+
+        if len(message_content) > 1000:
+            message_content = f"{message_content[:1000]}...[Continued]"
+
+        embed = discord.Embed(
+            title=f"*** Message Log Entry ***",
+            description=f"○○○○○○○○○○○○○○○○○○○○○○○○○○○",
+            color=config.success)
+        embed.add_field(name=f"Message Deleted - ", value=f"{message_content}", inline=False)
+        embed.set_footer(text=f"• {time.ctime(time.time())}")
+
+        try:
+            await self.bot.get_channel(send_channel).send(embed=embed)
+        except Exception as err:
+            print(f'An error has occurred: {err}')
+
+    @staticmethod
+    # DB serializer/deserializer
+    async def serialize_to_db(message):
+        """
+        Parameters
+        ----------
+            message : https://discordpy.readthedocs.io/en/latest/api.html?highlight=message#discord.Message
+                The message object being passed to serialize_to_db().
+                    author [Member, abc.User]
+                    channel [Channel]
+                    content [str]
+                    guild [GuildObject]
+                    id [int]
+        """
+        current_time = datetime.datetime.utcnow()
+
+        message_data = {
+            "_id": f"{message.id}",
+            "time": current_time,
+            "author_id": f"{message.author.id}",
+            "author_name": f"{message.author.name}",
+            "author_full_name": f"{message.author.name}#{message.author.discriminator}",
+            "channel_id": f"{message.channel.id}",
+            "server_name": f"{message.guild.name}",
+            "contents": f"{message.content}"
+        }
+
+        user_data = {"_id": f"{message.author.id}"}, {'$push': {"message_ids": f"{message.id}"}}, {'upsert': True}
+
+        message_sent = message_db.insert_one(message_data)
+        user_db.update_one(user_data)
+        print(f"Message logged - Message ID: {message_sent.inserted_id}")
+        return message_data
+
+    @staticmethod
+    async def deserialize_from_db(message):
+        """
+        Parameters
+        ----------
+            message : https://discordpy.readthedocs.io/en/latest/api.html?highlight=message#discord.Message
+                The message object being passed to deserialize_from_db().
+                    author [Member, abc.User]
+                    id [int]
+        """
+        x = message_db.find({"_id": f"{message.message_id}"})
+
+        return x
+
+    # Member counter ###################################################################################################
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+
+        user_data = {
+            "_id": f"{member.id}",
+            "linked_accounts": {
+                "minecraft": "none",
+                "twitch": "none",
+                "steam": "none"
+            },
+            "message_ids": [],
+            "joined_date": f"{datetime.datetime.utcnow()}"
+        }
+        try:
+            user_db.insert_one(user_data, upsert=True)
+
+        except pymongo.errors.WriteError:
+            print(f"User {member.name} already exists in the database with _id: {member.id}")
+        except Exception as err:
+            print(err)
+
+        welcome_messages = [
+            f"{member.name} is here to kick butt and chew bubblegum. And {member.name} is all out of gum.",
+            f"We've been expecting you {member.name}",
+            f"Big {member.name} showed up!",
+            f"Swoooosh. {member.name} just landed.",
+            f"Ermagherd. {member.name} is here."
+        ]
+
+        embed = discord.Embed(
+            title=f"{member.name} Joined!",
+            description=f"{random.choice(welcome_messages)}\n\n- Thank you for joining us {member.mention}",
+            color=config.level_up
+        )
+        embed.set_author(name=f"{member.name}", icon_url=member.avatar)
+        embed.add_field(name=f"Account Created:", value=f"<t:{member.created_at.strftime('%s')}:R>", inline=False)
+
+        if member.guild.id == 601677205445279744:
+            channel = member.guild.get_channel(619175785772875808)
+            await channel.edit(name=f'👦 Member Count: {member.guild.member_count}')
+
+            try:
+                channels = [743476824763269150, 601677205445279746]
+                for c in channels:
+                    await self.bot.get_channel(c).send(embed=embed)
+
+            except Exception as err:
+                print(f'An error has occurred: {err}')
+            print(f'{member} joined {member.guild.name} ( Current Members: {member.guild.member_count} )')
+
+        else:
+            await self.bot.get_channel(626084615660109824).send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = member.guild.get_channel(619175785772875808)
+        await channel.edit(name=f'👦 Member Count: {member.guild.member_count}')
+
+        embed = discord.Embed(
+            title=f"{member} Has left!",
+            description=f"The door hit {member} on the ass on the way out!",
+            color=config.error
+        )
+        embed.set_author(name=member.name, icon_url=member.avatar)
+        try:
+            await self.bot.get_channel(743476824763269150).send(embed=embed)
+        except Exception as err:
+            print(f'An error has occurred: {err}')
+        print(f'{member} left {member.guild.name} ( Current Members: {member.guild.member_count} )')
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(
+        EventLogger(bot),
+        guilds=[
+            discord.Object(id=626078288556851230)
+        ]
+    )
+
+
--- main.py
+++ main.py
@@ -5,13 +5,9 @@ from discord.ext import commands
 import os
 import sys
 import time
-import random
-from datetime import date
 import aiohttp
-import aiofiles
-import zipfile
-import json
-from discord import app_commands
+from discord import Interaction
+from discord.app_commands import AppCommandError
 
 # Does not allow bot to start without config file
 if not os.path.isfile("config.py"):
@@ -21,9 +17,9 @@ else:
 
 
 class GeneralBot(commands.Bot):
-    '''
+    """
     General bot for Adriftus.
-    '''
+    """
 
     def __init__(self):
         super().__init__(
@@ -47,8 +43,6 @@ class GeneralBot(commands.Bot):
 
         print(f'Full List: {self.initial_extensions}')
 
-        self.root = "/home/minecraft/bots/AdriftusGeneral/"
-
     async def setup_hook(self):
         for ext in self.initial_extensions:
             await self.load_extension(ext)
@@ -71,293 +65,14 @@ class GeneralBot(commands.Bot):
 bot = GeneralBot()
 
 
-async def zip_check():
-    current_time = time.ctime(time.time())
-    file_size = os.stat('message_log.txt')
-    if file_size.st_size >= 100000:
-        output = f'message_log[{current_time}].zip'
-        input = 'message_log.txt'
-        with zipfile.ZipFile(output, 'w', zipfile.ZIP_DEFLATED) as zip_f:
-            zip_f.write(input)
-        os.remove(input)
-        print(f"Logs have been compressed to: {output}")
-
-
-# Member counter
-@bot.event
-async def on_member_join(member):
-    welcome_messages = [
-        f"{member.name} is here to kick butt and chew bubblegum. And {member.name} is all out of gum.",
-        f"We've been expecting you {member.name}",
-        f"Big {member.name} showed up!",
-        f"Swoooosh. {member.name} just landed.",
-        f"Ermagherd. {member.name} is here."
-    ]
-
+# Error handling in chat
+@bot.tree.error
+async def on_app_command_error(itx: Interaction, error: AppCommandError):
     embed = discord.Embed(
-        title=f"{member.name} Joined!",
-        description=f"{random.choice(welcome_messages)}\n\n- Thank you for joining us {member.mention}",
-        color=config.level_up
-    )
-    embed.set_author(name=f"{member.name}", icon_url=member.avatar)
-    embed.add_field(name=f"Account Created:", value=f"<t:{member.created_at.strftime('%s')}:R>", inline=False)
-
-    if member.guild.id == 601677205445279744:
-        channel = member.guild.get_channel(619175785772875808)
-        await channel.edit(name=f'👦 Member Count: {member.guild.member_count}')
-
-        try:
-            channels = [743476824763269150, 601677205445279746]
-            for c in channels:
-                await bot.get_channel(c).send(embed=embed)
-
-        except Exception as err:
-            print(f'An error has occured: {err}')
-        print(f'{member} joined {member.guild.name} ( Current Members: {member.guild.member_count} )')
-
-    else:
-        await bot.get_channel(626084615660109824).send(embed=embed)
-
-
-@bot.event
-async def on_member_remove(member):
-    channel = member.guild.get_channel(619175785772875808)
-    await channel.edit(name=f'👦 Member Count: {member.guild.member_count}')
-
-    embed = discord.Embed(
-        title=f"{member} Has left!",
-        description=f"The door hit {member} on the ass on the way out!",
+        title="Error!",
+        description=f"This command raised an error: \n{error}",
         color=config.error
     )
-    embed.set_author(name=member.name, icon_url=member.avatar)
-    try:
-        await bot.get_channel(743476824763269150).send(embed=embed)
-    except Exception as err:
-        print(f'An error has occured: {err}')
-    print(f'{member} left {member.guild.name} ( Current Members: {member.guild.member_count} )')
-
-
-# Log Events --
-@bot.event
-async def on_raw_message_delete(payload):
-    admin_channels = [626086306606350366, 651870920599928862]
-    blacklist_channels = [965027742154358814]
-    date_as_path = await current_date_to_path()
-
-    mID = f"{payload.channel_id}_{payload.message_id}"
-
-    serialize_data = {
-        "author_id": f"{payload.cached_message.author.id}",
-        "author_name": f"{payload.cached_message.author.name}",
-        "author_full_name": f"{payload.cached_message.author.name}#{payload.cached_message.author.discriminator}",
-        "channel_id": f"{payload.cached_message.channel.id}",
-        "server_name": f"{payload.cached_message.guild.name}",
-        "contents": f"{payload.cached_message.content}"
-    }
-    try:
-        if payload.cached_message is not None:
-            serialize_data = serialize_data
-        elif os.path.isfile(f"{bot.root}cache/{date_as_path}/{mID}"):
-            serialize_data = await deserialize_from_disc(payload.channel_id, payload.message_id)
-
-        if payload.channel_id in admin_channels:
-            await message_sender(payload, serialize_data, 712309385019523155)
-        elif payload.channel_id in blacklist_channels:
-            await message_sender(payload, serialize_data, 965027742154358814)
-        else:
-            await message_sender(payload, serialize_data, 965027742154358814)
-    except Exception as err:
-        print(f'An error has occurred: {err}')
-
-
-@bot.event
-async def on_message(message):
-
-    if message.channel.id == 969954602051055636:
-        return
-
-    try:
-        await serialize_to_disk(message)
-
-        slash_command_channels = [677175985174478869]
-        if message.channel.id in slash_command_channels:
-            if not isinstance(message, discord.MessageType.chat_input_command) and not message.author.bot:
-                await bot.get_channel(965027742154358814).send("You are only allowed to send commands here.")
-                await message.delete()
-    except Exception as err:
-        print(f"An error has occurred: {err}")
-
-
-@bot.event
-async def on_guild_channel_delete(channel):
-    print(channel)
-
-
-# date path creator
-async def current_date_to_path():
-    current_date = date.today().strftime("%d")
-    current_month = date.today().strftime("%b")
-    current_year = date.today().strftime("%Y")
-    return f"{current_year}/{current_month}/{current_date}"
-
-
-# Path creator
-async def check_or_create_folder_structure(path):
-    parts = path.split("/")
-    base = bot.root
-    for part in parts:
-        if not os.path.exists(base + part):
-            os.mkdir(f"{base}{part}")
-        base = f"{base}{part}/"
-
-
-# Disk serializer/deserializer
-async def serialize_to_disk(message):
-    channel_id = message.channel.id
-    message_id = message.id
-
-    mID = f"{channel_id}_{message_id}"
-
-    date_as_path = await current_date_to_path()
-    await check_or_create_folder_structure("cache/"+date_as_path)
-    with open(f"cache/{date_as_path}/{mID}", "w") as file:
-        await dump(message, file)
-
-
-async def deserialize_from_disc(channel_id, message_id):
-    mID = f"{channel_id}_{message_id}"
-    date_as_path = await current_date_to_path()
-    if os.path.exists(f"cache/{date_as_path}/{mID}"):
-        with open(f"cache/{date_as_path}/{mID}", "r") as file:
-            return await load(file)
-
-
-async def dump(message, file):
-    serialize_data = {
-        "author_id": f"{message.author.id}",
-        "author_name": f"{message.author.name}",
-        "author_full_name": f"{message.author.name}#{message.author.discriminator}",
-        "channel_id": f"{message.channel.id}",
-        "server_name": f"{message.guild.name}",
-        "contents": f"{message.content}"
-    }
-    json.dump(serialize_data, file, indent=4)
-
-
-async def load(file):
-    return json.load(file)
-
-
-# Capability to add/remove blacklist channels, and change sent logs to what channel
-async def message_sender(payload, serialize_data, payload_guild_id):
-    current_time = time.ctime(time.time())
-    guild = bot.get_guild(payload.guild_id)
-
-    async for entry in guild.audit_logs(limit=1, action=discord.AuditLogAction.message_delete):
-        deleted_channel_id = entry.extra.channel.id
-        deleted_owner = entry.target.id
-        deleter = entry.user
-
-    if int(payload.channel_id) != int(deleted_channel_id) or int(serialize_data["author_id"]) != int(deleted_owner):
-        deleter = bot.get_user(int(serialize_data["author_id"]))
- 
-    message_content = f"""
-            *** Message Log Entry - {current_time} ***
-            - {serialize_data["author_full_name"]} had their message deleted -
-              **__Deleted By__**: {deleter.name}#{deleter.discriminator}
-              **__Message Author__**: {serialize_data["author_full_name"]}
-              **__Author ID__**: {serialize_data["author_id"]}
-              **__Channel Info__**: <#{serialize_data["channel_id"]}> / {serialize_data["channel_id"]}
-              **__Server Name__**: {serialize_data["server_name"]}
-
-              **__Message:__**\n{serialize_data["contents"]}
-            ----------------------------------------------------------------\n
-            """
-    async with aiofiles.open("message_log.txt", "a") as f:
-        await f.writelines(message_content)
-
-    if len(message_content) > 1000:
-            message_content = f"{message_content[:1000]}...[Continued]"
-
-    embed = discord.Embed(
-        title=f"*** Message Log Entry ***",
-        description=f"○○○○○○○○○○○○○○○○○○○○○○○○○○○",
-        color=config.success)
-    embed.add_field(name=f"Message Deleted - ", value=f"{message_content}", inline=False)
-    embed.set_footer(text=f"• {time.ctime(time.time())}")
-
-    try:
-        await bot.get_channel(payload_guild_id).send(embed=embed)
-    except Exception as err:
-        print(f'An error has occurred: {err}')
-    print(
-        f'{serialize_data["author_name"]} had their message deleted in {serialize_data["server_name"]} - Action logged')
-    await zip_check()
-
-
-@bot.event
-async def on_interaction(
-        ctx: discord.Interaction):
-    try:
-        executed_command = ctx.command.name
-        message_content = f"""
-        - Executed **{executed_command}** command in {ctx.guild} (ID: {ctx.guild.id}) by {ctx.user} (ID: {ctx.user.id})\n
-        ----------------------------------------------------------------\n
-        """
-        async with aiofiles.open("message_log.txt", "a") as f:
-            await f.writelines(message_content)
-
-        if len(message_content) > 1000:
-            message_content = f"{message_content[:1000]}...[Continued]"
-
-        embed = discord.Embed(
-            title=f"*** Command Log Entry ***",
-            description=f"○○○○○○○○○○○○○○○○○○○○○○○○○○○",
-            color=config.success)
-        embed.add_field(name=f"Command Executed - ", value=f"{message_content}", inline=False)
-        embed.set_footer(text=f"• {time.ctime(time.time())}")
-
-        try:
-            await bot.get_channel(965027742154358814).send(embed=embed)
-        except Exception as err:
-            print(f'An error has occured: {err}')
-        print(f"Executed {executed_command} command in {ctx.guild} (ID: {ctx.guild.id}) by {ctx.user} (ID: {ctx.user.id})")
-        await zip_check()
-    except Exception as err:
-        print(err)
-        await zip_check()
-
-
-# Error handling in chat
-@bot.event
-async def on_command_error(
-        ctx: discord.Interaction,
-        error):
-    if isinstance(error, app_commands.errors.CommandOnCooldown):
-        embed = discord.Embed(
-            title="Error!",
-            description=f"This command is on a {error.retry_after}s cool down",
-            color=config.error
-        )
-        await ctx.response.send_message(embed=embed)
-
-    elif isinstance(error, commands.errors.MissingRequiredArgument):
-        embed = discord.Embed(
-            title='Error!',
-            description=f'Parameter missing: {commands.errors.MissingRequiredArgument.args}',
-            color=config.error
-        )
-        await ctx.response.send_message(embed=embed)
-
-    elif isinstance(error, commands.errors.CommandInvokeError):
-        embed = discord.Embed(
-            title='Error!',
-            description=f'{error}',
-            color=config.error
-        )
-        await ctx.response.send_message(embed=embed)
-
-    elif not isinstance(error, commands.errors.CommandNotFound):
-        raise error
+    await itx.response.send_message(embed=embed)
 
 bot.run(secrets.TOKEN)
